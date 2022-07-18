ba5747f86787ac8a02393385008ed8e1f1976201
Kingdomms Corp <hazael.flores22@icloud.com>
2022-07-18 14:21:11 -0500

    Programacion de script para la animacion del menu

---
+++ main/scripts/script.js
@@ -0,0 +1,10 @@
+/* Archivo de Scripts (Javascript) */
+
+/* Script para animacion del menu en dispositivos moviles */
+var navLinks = document.getElementById("navLinks");
+function showMenu() {
+  navLinks.style.right = "0";
+}
+function hideMenu() {
+  navLinks.style.right = "-200px";
+}
