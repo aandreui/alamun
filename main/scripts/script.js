d0859fa345bb6abb0d5eea4556b0cceb54d4254c
Kingdomms <hazael.flores22@icloud.com>
2023-05-19 22:35:19 -0600

    Cambios en timer

--- main/scripts/script.js
+++ main/scripts/script.js
@@ -12,3 +12,34 @@ function hideMenu() {
 function notavailable() {
   alert("This information is not yet available. Try later.");
 }
+
+// Timer Function
+
+function startTimer() {
+  var hours = parseInt(document.getElementById("hoursInput").value) || 0;
+  var minutes = parseInt(document.getElementById("minutesInput").value) || 0;
+  var seconds = parseInt(document.getElementById("secondsInput").value) || 0;
+
+  var totalSeconds = hours * 3600 + minutes * 60 + seconds;
+
+  var timer = setInterval(function () {
+    var hoursDisplay = Math.floor(totalSeconds / 3600);
+    var minutesDisplay = Math.floor((totalSeconds % 3600) / 60);
+    var secondsDisplay = Math.floor(totalSeconds % 60);
+
+    document.getElementById("hours").textContent = padZero(hoursDisplay);
+    document.getElementById("minutes").textContent = padZero(minutesDisplay);
+    document.getElementById("seconds").textContent = padZero(secondsDisplay);
+
+    if (totalSeconds <= 0) {
+      alert("Time Up!");
+      clearInterval(timer);
+    } else {
+      totalSeconds--;
+    }
+  }, 1000);
+}
+
+function padZero(num) {
+  return (num < 10 ? "0" : "") + num;
+}
