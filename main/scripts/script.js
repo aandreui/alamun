293549648716cfb5790259d5334f16d32e4ddf02
Kingdomms <hazael.flores22@icloud.com>
2023-07-30 17:21:15 -0600

    Animations Added

--- main/scripts/script.js
+++ main/scripts/script.js
@@ -43,3 +43,203 @@ function startTimer() {
 function padZero(num) {
   return (num < 10 ? "0" : "") + num;
 }
+
+{
+  // Animation Functions Text
+
+  const textElement = document.querySelector(".animated-text");
+
+  // Function to check if the element is in the viewport
+  const isElementInViewport = (el) => {
+    const rect = el.getBoundingClientRect();
+    return (
+      rect.top >= 0 &&
+      rect.left >= 0 &&
+      rect.bottom <=
+        (window.innerHeight || document.documentElement.clientHeight) &&
+      rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+  };
+
+  // Callback function when the element is in the viewport
+  const handleElementIntersection = (entries, observer) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        // Element is now visible on the screen, apply the animations
+        entry.target.style.animation =
+          "tracking-in-expand 0.7s cubic-bezier(0.215, 0.61, 0.355, 1) 1s both";
+      }
+    });
+  };
+
+  // Options for the Intersection Observer
+  const options = {
+    threshold: 0.5, // 0.5 means 50% of the element needs to be visible to trigger the animation
+  };
+
+  // Create the Intersection Observer
+  const observer = new IntersectionObserver(handleElementIntersection, options);
+
+  // Start observing the text element
+  observer.observe(textElement);
+}
+
+{
+  // Animation Functions Text
+
+  const textElement = document.querySelector(".animated-text2");
+
+  // Function to check if the element is in the viewport
+  const isElementInViewport = (el) => {
+    const rect = el.getBoundingClientRect();
+    return (
+      rect.top >= 0 &&
+      rect.left >= 0 &&
+      rect.bottom <=
+        (window.innerHeight || document.documentElement.clientHeight) &&
+      rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+  };
+
+  // Callback function when the element is in the viewport
+  const handleElementIntersection = (entries, observer) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        // Element is now visible on the screen, apply the animations
+        entry.target.style.animation =
+          "tracking-in-expand 0.7s cubic-bezier(0.215, 0.61, 0.355, 1) 1s both";
+      }
+    });
+  };
+
+  // Options for the Intersection Observer
+  const options = {
+    threshold: 0.5, // 0.5 means 50% of the element needs to be visible to trigger the animation
+  };
+
+  // Create the Intersection Observer
+  const observer = new IntersectionObserver(handleElementIntersection, options);
+
+  // Start observing the text element
+  observer.observe(textElement);
+}
+
+{
+  // Animation Functions Boxes
+
+  const textElement = document.querySelector(".info1");
+
+  // Function to check if the element is in the viewport
+  const isElementInViewport = (el) => {
+    const rect = el.getBoundingClientRect();
+    return (
+      rect.top >= 0 &&
+      rect.left >= 0 &&
+      rect.bottom <=
+        (window.innerHeight || document.documentElement.clientHeight) &&
+      rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+  };
+
+  // Callback function when the element is in the viewport
+  const handleElementIntersection = (entries, observer) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        // Element is now visible on the screen, apply the animations
+        entry.target.style.animation =
+          "scale-in-ver-top 0.8s cubic-bezier(0.25, 0.46, 0.45, 0.94) 2s both";
+      }
+    });
+  };
+
+  // Options for the Intersection Observer
+  const options = {
+    threshold: 0.0, // 0.5 means 50% of the element needs to be visible to trigger the animation
+  };
+
+  // Create the Intersection Observer
+  const observer = new IntersectionObserver(handleElementIntersection, options);
+
+  // Start observing the text element
+  observer.observe(textElement);
+}
+
+{
+  // Animation Functions Boxes
+
+  const textElement = document.querySelector(".info2");
+
+  // Function to check if the element is in the viewport
+  const isElementInViewport = (el) => {
+    const rect = el.getBoundingClientRect();
+    return (
+      rect.top >= 0 &&
+      rect.left >= 0 &&
+      rect.bottom <=
+        (window.innerHeight || document.documentElement.clientHeight) &&
+      rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+  };
+
+  // Callback function when the element is in the viewport
+  const handleElementIntersection = (entries, observer) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        // Element is now visible on the screen, apply the animations
+        entry.target.style.animation =
+          "scale-in-ver-top 0.8s cubic-bezier(0.25, 0.46, 0.45, 0.94) 3s both";
+      }
+    });
+  };
+
+  // Options for the Intersection Observer
+  const options = {
+    threshold: 0.0, // 0.5 means 50% of the element needs to be visible to trigger the animation
+  };
+
+  // Create the Intersection Observer
+  const observer = new IntersectionObserver(handleElementIntersection, options);
+
+  // Start observing the text element
+  observer.observe(textElement);
+}
+
+{
+  // Animation Functions Boxes
+
+  const textElement = document.querySelector(".info3");
+
+  // Function to check if the element is in the viewport
+  const isElementInViewport = (el) => {
+    const rect = el.getBoundingClientRect();
+    return (
+      rect.top >= 0 &&
+      rect.left >= 0 &&
+      rect.bottom <=
+        (window.innerHeight || document.documentElement.clientHeight) &&
+      rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+  };
+
+  // Callback function when the element is in the viewport
+  const handleElementIntersection = (entries, observer) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        // Element is now visible on the screen, apply the animations
+        entry.target.style.animation =
+          "scale-in-ver-top 0.8s cubic-bezier(0.25, 0.46, 0.45, 0.94) 4s both";
+      }
+    });
+  };
+
+  // Options for the Intersection Observer
+  const options = {
+    threshold: 0.0, // 0.5 means 50% of the element needs to be visible to trigger the animation
+  };
+
+  // Create the Intersection Observer
+  const observer = new IntersectionObserver(handleElementIntersection, options);
+
+  // Start observing the text element
+  observer.observe(textElement);
+}
